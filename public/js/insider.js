26199dea6c8484fec7358a3903614b1d64f647d3
David Szabo <david_szabo@mentor.com>
2020-06-18 21:58:41 +0200

    Mostly transferred to client-side

--- public/js/insider.js
+++ public/js/insider.js
@@ -1,21 +1,30 @@
 $(function () {
     var socket = io();
-    var word_shown = false;
-    $('form').submit(function(e) {
+
+    $('.new_word').click(function(e) {
       e.preventDefault();
-      if (word_shown) {
-        $('.new_word').text('Új szó');
-        $('.word').text('');
-        word_shown = false;
-      }
-      else {
-        socket.emit('get_word');
-      }
+      socket.emit('get_word');
       return false;
     });
-    socket.on('new_word', function(msg) {
-      $('.new_word').text('Szó elrejtése');
+
+    $('.hide_show_word').click(function(e) {
+      e.preventDefault();
+      if ($('.word').is(':visible')) {
+        $('.word').hide();
+        $('.hide_show_word').text('Mutasd');
+      }
+      else {
+        $('.word').show();
+        $('.hide_show_word').text('Rejtsd el');
+      }
+      $('.new_word').show();
+      return false;
+    });
+
+    socket.on('new_word_sent', function(msg) {
       $('.word').text(msg);
-      word_shown = true;
+      $('.word').show();
+      $('.new_word').hide();
+      $('.hide_show_word').text('Szó elrejtése');
     });
 });
--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const words = require('./resources/insider/szavak.json');
 io.on('connection', (socket) => {
 	socket.on('get_word', () => {
 		var word = words.words[Math.floor(Math.random() * words.words.length)];
-		io.emit('new_word', word);
+		io.emit('new_word_sent', word);
 	});
 });
 
